d7a78be644930c896c10c9db14d520569f6bfd62
D N <danuser2018@gmail.com>
2022-04-24 00:55:43 +0200

    function to get the hash of a block

--- src/main/kotlin/blockchain/domain/ProofOfWork.kt
+++ src/main/kotlin/blockchain/domain/ProofOfWork.kt
@@ -4,16 +4,16 @@ tailrec fun proofOfWork(
     previousProof: Double,
     newProof: Double = 1.0,
     complexOperation: (Double, Double) -> Double,
-    sha256: (Double) -> String,
+    toHash: (Double) -> String,
     isValid: (String) -> Boolean
 ): Double {
-    val hash = sha256(complexOperation(newProof, previousProof))
+    val hash = toHash(complexOperation(newProof, previousProof))
     return when {
         isValid(hash) -> newProof
         else -> proofOfWork(
             previousProof = previousProof,
             newProof = newProof + 1.0,
-            sha256 = sha256,
+            toHash = toHash,
             complexOperation = complexOperation,
             isValid = isValid
         )
